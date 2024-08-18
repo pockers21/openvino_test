c90b8e63aec96f589274bc0dafd8558b277cb29d
pockers21 <134406831+pockers21@users.noreply.github.com>
2024-08-18 22:49:21 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -1,30 +1,34 @@
-set -e
+#!/bin/bash
 set -x
 mode="ans"
+
+
 declare -A normal_exe_script_dict
-
-
-normal_exe_script_dict["ans"]="infer_asn.py"
+normal_exe_script_dict["ans"]="infer_ans.py"
 normal_exe_script_dict["fc"]="infer_fc.py"
 
 
 base_data_dir="/home/uos/openvino_test/openvino_model_7b/"
+
 model_dirs=(
-    uos_7b_cache_false_dir=${base_data_dir}uos_july_7b_818_cache_false
-    uos_7b_cache_true_dir=${base_data_dir}uos_july_7b_818_cache_true
-    qwen2_7b_instruct_cache_false_dir=${base_data_dir}qwen2_7b_instruct_818_cache_false
-    qwen2_7b_instruct_cache_true_dir=${base_data_dir}qwen2_7b_instruct_818_cache_true
-    ）
+    ${base_data_dir}uos_july_7b_818_cache_false
+    #{base_data_dir}uos_july_7b_818_cache_true
+    #${base_data_dir}qwen2_7b_instruct_818_cache_false
+    #${base_data_dir}qwen2_7b_instruct_818_cache_true
+)
 
 
-cd ${base_code_dir}
+cd ${base_data_dir}
 script=${normal_exe_script_dict[$mode]}
+echo ${script}
+
+
 for model_dir in "${model_dirs[@]}"; do
     # 检查缓存情况
     if [[ "$model_dir" == *"cache_false" ]]; then
         #qwen2 7b
         #cache false
-        python ${script} --do_sample --device="GPU"  --model $model_dir 
+        python ${script} --do_sample --device="GPU"  --model $model_dir
         python ${script} --do_sample --device="CPU"  --model $model_dir
         python ${script}  --device="GPU"     --model $model_dir
         python ${script}  --device="CPU"     --model $model_dir
@@ -40,13 +44,13 @@ done
 
 
 base_genai_code_dir="/home/uos/openvino_test/openvino.genai/samples/python/"
-greedy_dir=${base_code_dir}"greedy_causal_lm/"
-beam_dir=${base_code_dir}"beam_search_causal_lm/"
+greedy_dir=${base_genai_code_dir}greedy_causal_lm
+beam_dir=${base_genai_code_dir}beam_search_causal_lm
 
 declare -A genai_exe_script_dict
 
 for model_dir in "${model_dirs[@]}"; do
-    cd greedy_dir
+    cd ${greedy_dir}
     #cache false
     if [[ "$mode" == "ans" ]]; then
         script="greedy_causal_lm.py"
@@ -55,8 +59,8 @@ for model_dir in "${model_dirs[@]}"; do
     fi
     python ${script} $model_dir "Why is the Sun yellow?" CPU
     python ${script} $model_dir "Why is the Sun yellow?" GPU
-    
-    cd beam_dir
+
+    cd ${beam_dir}
     if [[ "$mode" == "ans" ]]; then
         script="beam_search_causal_lm.py"
     else
@@ -64,15 +68,6 @@ for model_dir in "${model_dirs[@]}"; do
     fi
     python ${script} $model_dir "Why is the Sun yellow?" CPU
     python ${script} $model_dir "Why is the Sun yellow?" GPU
-        
-    
+
+
 done
-
-
-
-
-
-
-
-
-
